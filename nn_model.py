45f56159fe2838284158aab6815bfd6e51664221
Francesca Stefano <84390854+Infraste03@users.noreply.github.com>
2023-12-01 12:02:29 +0100

    Update nn_model.py

--- nn_model.py
+++ nn_model.py
@@ -21,10 +21,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-##FRANCESCA RICORDATI CHE DEVI FATE LA GRIDH SEARCH, QUANDO HAI TROVATO I PAAMENTRI MIGLIORI ALLORA òI SOSTITUOSCI NEL MODELLI
-#POI FAI LA KFOLD VALIDATION E E CONFRONTI I TRE DIFFERENTI MODELLI PER VEDERE QUALE SIA MIGLIROE 
-#MAGARI FALLO IN UN FOR IN QUESTO MODO CONTRONTIT IN UN UNICA BOTTA 
-#DEVI FARTI STAMPERE IL VALORE PREDETTO TIPO O IN UN QUALCHE MODO FARE COSì
 
 # Tree Visualisation
 from sklearn.tree import export_graphviz
@@ -46,6 +42,18 @@ dfValidation.dropna(inplace=True)
 ######
 #PER LA VISUALIZZAZIONE
 #####
+dfTrain = dfTrain.drop(['BLE_tot_BO_time', 'BLE_tot_RR_time', 'Tempo Tot BO Ormaweb', 'Tempo Tot. SO OrmaWeb','Tempo Tot. RR', 'LATERALITA_BILATERALE','MALLAMPATI_4.0', 'TORACOSCOPIA','MIVAR', 'PERCUTANEO','OSAS', 'ENDOSCOPIA', 
+                        'Antipertensivi', 'Insulina', 'Reintervento', 'Fumo', 'CVC', 'BPCO', 'feasible', 'TIGO', 'Anticoagulanti', 'Pregresso TIA', 'Codice alfa numerico'], axis=1)
+
+dfTest = dfTest.drop(['BLE_tot_BO_time', 'BLE_tot_RR_time', 'Tempo Tot BO Ormaweb', 'Tempo Tot. SO OrmaWeb','Tempo Tot. RR', 'LATERALITA_BILATERALE', 'MALLAMPATI_4.0', 
+                      'TORACOSCOPIA','MIVAR','PERCUTANEO','OSAS','ENDOSCOPIA', 'Antipertensivi','Insulina', 'Reintervento','Fumo', 'CVC', 'BPCO', 'feasible', 'TIGO'
+                      , 'Anticoagulanti', 'Pregresso TIA', 'Codice alfa numerico'], axis=1)
+
+
+dfValidation = dfValidation.drop(['BLE_tot_BO_time', 'BLE_tot_RR_time', 'Tempo Tot BO Ormaweb', 'Tempo Tot. SO OrmaWeb','Tempo Tot. RR',
+                                  'LATERALITA_BILATERALE','MALLAMPATI_4.0', 'TORACOSCOPIA','MIVAR', 'PERCUTANEO','OSAS', 'ENDOSCOPIA', 
+                                  'Antipertensivi', 'Insulina', 'Reintervento', 'Fumo', 'CVC', 'BPCO', 'feasible',
+                                  'TIGO', 'Anticoagulanti', 'Pregresso TIA', 'Codice alfa numerico'], axis=1)
 
 le = LabelEncoder()
 for col in dfTrain.columns:
@@ -76,16 +84,25 @@ from keras.layers import Dense
 import numpy as np
 import tensorflow as tf
 from keras.callbacks import EarlyStopping,ReduceLROnPlateau
+from keras import backend as K
+from keras.layers import Dropout
 
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true))) 
+
+# Define the model
 nn_model = Sequential()
-nn_model.add(Dense(100, input_shape=(62,), activation='relu'))
+nn_model.add(Dense(100, input_shape=(39,), activation='relu'))
 nn_model.add(Dense(100, activation='relu'))
+nn_model.add(Dense(100, activation='relu'))  # Added layer
+nn_model.add(Dense(100, activation='relu'))  # Added layer
 nn_model.add(Dense(1, activation='linear'))
 
 # Compile the model
-nn_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+nn_model.compile(loss=root_mean_squared_error, optimizer='adam', metrics=[root_mean_squared_error])
 
-# Convert the data to tensors
+# Convert data to tensors
 x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
 y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
 x_val = tf.convert_to_tensor(x_val, dtype=tf.float32)
@@ -93,21 +110,13 @@ y_val = tf.convert_to_tensor(y_val, dtype=tf.float32)
 x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
 y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
 
-# Define the callbacks
+# Define callbacks
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min', restore_best_weights=True)
 lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='min', min_lr=1e-6)
 
-# Fit the model with callbacks
+# Train the model
 history = nn_model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=50, callbacks=[early_stopping, lr_scheduler])
 
 # Evaluate the model
 scores = nn_model.evaluate(x_test, y_test)
-print("Mean Absolute Error: ", scores[1])
-
-# Plot the learning curves
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='validation')
-plt.legend()
-plt.show()
-
-
+print("RMSE:", scores[1])
